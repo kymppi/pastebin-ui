8dba7374c2279f5f48e2262ed6e9d20a585b049a
Midka <me@midka.dev>
2023-02-22 15:54:10 +0200

    add: tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node()
+  adapter: node(),
+  integrations: [tailwind()]
 });
