fea8c645c3ee6e9a0f94cc6a8d44245468707f60
Midka <me@midka.dev>
2023-02-22 17:33:01 +0200

    add: link styling and colorscheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8ecae6',
+        secondary: '#219ebc',
+        accent: '#ffb703',
+        'accent-2': '#fb8500',
+        background: '#023047',
+      },
+    },
+  },
+  plugins: [],
+};
