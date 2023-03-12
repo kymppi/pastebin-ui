ca008e3a3c2700d12440d51c518c5ebc6b4a219d
Midka <me@midka.dev>
2023-03-12 12:32:03 +0200

    fix: util not found for axios

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge-functions';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  vite: {
+    plugins: [
+      nodePolyfills({
+        protocolImports: true,
+      }),
+    ],
+  },
   integrations: [tailwind()],
 });
